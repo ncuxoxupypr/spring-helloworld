5cdf1c581a37819b0e667ee2f0bcff3679cfac74
ncuxoxupypr <eu.kondratev@yandex.ru>
2021-03-24 22:27:26 +0300

    add class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean(Cat.class);
+        Cat cat1 = (Cat) applicationContext.getBean(Cat.class);
+
+        System.out.println("equals one " + (bean == bean1));
+        System.out.println("equals two " + (cat == cat1));
+
         System.out.println(bean.getMessage());
     }
 }
